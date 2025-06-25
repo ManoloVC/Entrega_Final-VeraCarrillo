ab89e61c506fedd30782214cc528fa03deb8257b
Manuel Vera Carrillo <manuvcarrillo@gmail.com>
2025-06-25 00:27:16 -0300

    Eliminar 'django.contrib.sites' de INSTALLED_APPS y mejorar el formulario de cierre de sesión en la plantilla base.

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'usuarios',
     'pages',
-    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = '/'
+
